5cfadcfd115008fc6370a996c7fcdf4a2d24d6f9
Siddharth <037.siddharth@gmail.com>
2024-08-01 15:07:50 +0530

    Update fetchuser.js

--- server/middlewares/fetchuser.js
+++ server/middlewares/fetchuser.js
@@ -2,7 +2,8 @@ const jwt = require('jsonwebtoken');
 const JWT_SECRET = 'termiinatorx'
 
 const fetchUser = (req, res, next) => {
-    const token = req.get('auth-token');
+    console.log(req.headers)
+    const token = req.header('auth-token');
 
     if (!token) {
         return res.status(401).json({ error: 'Access denied, no token provided' });
