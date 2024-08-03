94391f99d7e005196e9933c4d073e174cf71fcd2
Siddharth <037.siddharth@gmail.com>
2024-08-04 02:39:33 +0530

    Update assets.js

--- client/src/assets.js
+++ client/src/assets.js
@@ -1,4 +1,4 @@
 // export const url = "https://notesme-backend-ogr1.onrender.com"
 // export const url = "http://localhost:8080"
 // export const url = "https://e800d4eb275b9c.lhr.life"
-export const url = "https://notes-pnx85tda4-siddharth-ghs-projects.vercel.app"
+export const url = "https://notes-me-omega.vercel.app/"
