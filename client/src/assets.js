722bc51b6f233402ae2648ff7b9fffc9322295d6
Siddharth <119670710+siddharth-gh@users.noreply.github.com>
2024-08-04 02:09:10 +0530

    Update assets.js

--- client/src/assets.js
+++ client/src/assets.js
@@ -1,3 +1,4 @@
-export const url = "https://notesme-backend-ogr1.onrender.com"
+// export const url = "https://notesme-backend-ogr1.onrender.com"
 // export const url = "http://localhost:8080"
 // export const url = "https://e800d4eb275b9c.lhr.life"
+export const url = "https://notes-cjbnpr9vc-siddharth-ghs-projects.vercel.app"
